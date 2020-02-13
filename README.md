2a9a2df8f343ef517c07ed0c20662fcf606dab8a
Rui Ying <yingrui205@gmail.com>
2020-02-13 21:15:48 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# shadowsocks-libev-portable
+
+![linux](https://github.com/robertying/shadowsocks-libev-portable/workflows/linux/badge.svg)
+![mac](https://github.com/robertying/shadowsocks-libev-portable/workflows/mac/badge.svg)
+![windows](https://github.com/robertying/shadowsocks-libev-portable/workflows/windows/badge.svg)
+
+Portable build for [shadowsocks-libev](https://github.com/shadowsocks/shadowsocks-libev).
---
+++ build-windows.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+BUILD_DIR=$DIR/build
+mkdir -p $BUILD_DIR
+cd $BUILD_DIR
+
+GCC_PATH=$(which gcc)
+MINGW64_BIN=${GCC_PATH::-4}
+CPU=2
+
+pacman -S --noconfirm base-devel mingw-w64-x86_64-toolchain mingw-w64-x86_64-libsodium mingw-w64-x86_64-mbedtls mingw-w64-x86_64-pcre mingw-w64-x86_64-c-ares git
+
+git clone https://github.com/shadowsocks/shadowsocks-libev
+cd shadowsocks-libev
+git submodule init && git submodule update
+
+ver=4.31
+wget http://dist.schmorp.de/libev/libev-$ver.tar.gz
+tar zxf libev-$ver.tar.gz
+cd libev-$ver
+./configure --prefix=$BUILD_DIR/libev --disable-shared
+LDFLAGS=-static make -j$CPU
+make install
+cd ..
+
+./autogen.sh
+./configure --prefix=$BUILD_DIR/shadowsocks-libev --disable-documentation \
+--with-ev=$BUILD_DIR/libev
+make -j$CPU && make install
+cd ..
+
+cd ..
+
+cp $MINGW64_BIN/libwinpthread-1.dll $BUILD_DIR/shadowsocks-libev/bin/
+cp $MINGW64_BIN/libmbedcrypto.dll $BUILD_DIR/shadowsocks-libev/bin/
+cp $MINGW64_BIN/libssp-0.dll $BUILD_DIR/shadowsocks-libev/bin/
+cp $MINGW64_BIN/libpcre-1.dll $BUILD_DIR/shadowsocks-libev/bin/
+cp $MINGW64_BIN/libsodium-23.dll $BUILD_DIR/shadowsocks-libev/bin/
+cp $MINGW64_BIN/libgcc_s_seh-1.dll $BUILD_DIR/shadowsocks-libev/bin/
+
+tar czf shadowsocks-libev-windows.tar.gz $BUILD_DIR/shadowsocks-libev/bin
